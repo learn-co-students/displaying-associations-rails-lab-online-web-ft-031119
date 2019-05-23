a7adbb191cd6f8ead6892c9ae734e6fecc77fea5
Annie Phan <anniephan.atp@gmail.com>
2019-05-22 20:00:12 -0700

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Artist.create(name: "Halsey")
+Artist.create(name: "Gryffin")
+
+Song.create(title: "Nightmare", artist_id: "1")
+Song.create(title: "Eastside", artist_id: "1")
+Song.create(title: "11 Minutes", artist_id: "1")
+Song.create(title: "Love in Ruins", artist_id: "2")
+Song.create(title: "Nobody Compares to You", artist_id: "2")
